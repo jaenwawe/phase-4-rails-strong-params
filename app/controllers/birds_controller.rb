b3df68ca6952e6b39e507c0de239d0e08629e306
jaenwawe <nwawe.jae@gmail.com>
2021-10-11 19:09:47 -0500

    commit tree 09bbaa20080f8792f04cf3ab0ba709b641a1753d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,11 +6,8 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
+
+  
 
   # GET /birds/:id
   def show
@@ -21,5 +18,23 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+
+
+    # POST /birds
+    def create
+      bird = Bird.create(params.permit(:name, :species))
+      render json: bird, status: :created
+    end
+
+    
+   # other controller actions here
+
+   private
+   # all methods below here are private
+ 
+   def bird_params
+     params.permit(:name, :species)
+   end
 
 end
